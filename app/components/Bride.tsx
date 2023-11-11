257438e30c11bfc7f94ab08430bf91bd06f21075
Darrell-Rafa <drafa0609@gmail.com>
2023-11-11 22:55:52 +0700

    save the date desktop

--- app/components/Bride.tsx
+++ app/components/Bride.tsx
@@ -7,33 +7,59 @@ function Bride() {
     <section id="bride" className="relative">
       <div className="absolute inset-0 bg-[url('/assets/bride-bg.svg')] bg-cover"></div>
       <div className="absolute inset-0 bg-[linear-gradient(180deg,rgba(0,60,76,0.78)_0%,rgba(0,60,76,0.85)_74.88%,#003C4C_109.92%)]"></div>
-      <div className="text-[#D5AF6F] z-20 relative">
-        <div className="h-[15rem] bg-[url('/assets/groom.svg')] bg-cover bg-no-repeat"></div>
-        <div className="py-12 flex flex-col items-center gap-16 font-bold">
-          <div className="space-y-4 text-center">
-            <h1 className="text-4xl">Muhamad Agy Nurwicaksono</h1>
-            <h3 className="text-2xl">Putra dari Agus dan Evy</h3>
+      <div className="text-[#D5AF6F] z-20 relative lg:grid lg:grid-cols-2 lg:h-screen">
+        <span className="text-[#D5AF6F] text-3xl font-semibold absolute bottom-8 left-8 hidden lg:block z-10">
+          Putra dari
+          <br />
+          Bapak Agus
+          <br />
+          Ibu Evy
+        </span>
+        <span className="text-[#D5AF6F] text-3xl font-semibold absolute bottom-8 right-8 hidden lg:block z-10 text-right">
+          Putra dari
+          <br />
+          Bapak Agus
+          <br />
+          Ibu Tuti
+        </span>
+        <div className="h-full lg:bg-[url('/assets/groom.svg')] lg:bg-cover lg:bg-no-repeat lg:bg-center relative lg:flex lg:items-center lg:justify-center">
+          <div className="absolute inset-0 bg-black/50 hidden lg:block"></div>
+          <div className="h-[15rem] bg-[url('/assets/groom.svg')] bg-cover bg-no-repeat lg:hidden"></div>
+          <div className="py-12 flex flex-col items-center gap-16 font-bold z-10 relative">
+            <div className="space-y-4 text-center">
+              <h1 className="text-4xl lg:text-5xl leading-[1.25]">
+                Muhamad Agy
+                <br />
+                Nurwicaksono
+              </h1>
+              <h3 className="text-2xl lg:hidden">Putra dari Agus dan Evy</h3>
+              <Link
+                href="https://www.instagram.com/agyson"
+                className="flex items-center justify-center gap-2"
+              >
+                <InstagramLogo size={32} />
+                <span className="text-2xl lg:font-normal">agyson</span>
+              </Link>
+            </div>
+            <span className="text-8xl lg:hidden">&</span>
+          </div>
+        </div>
+        <div className="h-full lg:bg-[url('/assets/bride.svg')] lg:bg-cover lg:bg-no-repeat lg:bg-center relative lg:flex lg:items-center lg:justify-center">
+          <div className="absolute inset-0 bg-black/50 hidden lg:block"></div>
+          <div className="h-[15rem] bg-[url('/assets/bride.svg')] bg-cover bg-no-repeat lg:hidden"></div>
+          <div className="py-12 font-bold space-y-4 text-center z-10 relative">
+            <h1 className="text-4xl lg:text-5xl">
+              Yoriko <br className="hidden lg:inline-block" /> Angeline
+            </h1>
+            <h3 className="text-2xl lg:hidden">Putri dari Agus dan Ibu Tuti</h3>
             <Link
-              href="https://www.instagram.com/agyson"
+              href="https://www.instagram.com/yorikoangeline"
               className="flex items-center justify-center gap-2"
             >
               <InstagramLogo size={32} />
-              <span className="text-2xl">agyson</span>
+              <span className="text-2xl lg:font-normal">yorikoangeline</span>
             </Link>
           </div>
-          <span className="text-8xl ">&</span>
-        </div>
-        <div className="h-[15rem] bg-[url('/assets/bride.svg')] bg-cover bg-no-repeat"></div>
-        <div className="py-12 font-bold space-y-4 text-center">
-          <h1 className="text-4xl">Yoriko Angeline</h1>
-          <h3 className="text-2xl">Putri dari Agus dan Ibu Tuti</h3>
-          <Link
-            href="https://www.instagram.com/yorikoangeline"
-            className="flex items-center justify-center gap-2"
-          >
-            <InstagramLogo size={32} />
-            <span className="text-2xl">yorikoangeline</span>
-          </Link>
         </div>
       </div>
     </section>
--- app/components/DetailDate.tsx
+++ app/components/DetailDate.tsx
@@ -1,62 +1,81 @@
+import Image from "next/image";
 import React from "react";
 import { lovelyCoffee } from "../fonts";
 
 function DetailDate() {
   return (
     <section id="detail-date" className="relative">
-      <div className="absolute bottom-0 inset-x-0 h-[125px] bg-[url('/assets/date-flower.png')]"></div>
-      <div className="px-6 pt-12 pb-40 text-[#D5AF6F]">
+      <div className="absolute inset-0 bg-[url('/assets/date-bg.svg')] bg-no-repeat bg-cover bg-center hidden lg:block"></div>
+      <div className="absolute inset-0 bg-[linear-gradient(180deg,rgba(0,60,76,0.78)_0%,rgba(0,60,76,0.85)_74.88%,#003C4C_109.92%)] hidden lg:block"></div>
+      <Image
+        src="/assets/date-flower-left.svg"
+        alt="flower"
+        width={250}
+        height={250}
+        className="absolute bottom-0 left-0 hidden lg:inline-block"
+      />
+      <Image
+        src="/assets/date-flower-right.svg"
+        alt="flower"
+        width={250}
+        height={250}
+        className="absolute bottom-0 right-0 hidden lg:inline-block"
+      />
+      <div className="absolute bottom-0 inset-x-0 h-[125px] bg-[url('/assets/date-flower.png')] lg:hidden"></div>
+      <div className="px-6 lg:px-32 pt-12 lg:pt-32 pb-40 lg:pb-48 text-[#D5AF6F] z-10 relative">
         <h1 className={`text-center text-8xl mb-8 ${lovelyCoffee.className}`}>
-          Save
-          <br />
+          Save <br className="lg:hidden" />
           The Date
         </h1>
-        <h3 className="text-center text-[4.25rem] leading-[1] font-light mb-16 text-[#84A7A1]">
+        <h3 className="text-center text-[4.25rem] lg:text-[5rem] leading-[1] font-light mb-16 text-[#84A7A1]">
           00:00:00:00
         </h3>
-        <div className="grid grid-cols-2 text-xl mb-10">
-          <button className="border-r-2 border-b-2 pb-3 border-[#84A7A1]">
+        <div className="grid grid-cols-2 text-xl lg:text-3xl mb-10 lg:mb-16">
+          <button className="border-r-2 border-b-2 pb-3 lg:pb-4 border-[#84A7A1]">
             Resepsi Virtual
           </button>
-          <button className="border-b-2 pb-3 border-[#84A7A1]">Resepsi</button>
+          <button className="border-b-2 pb-3 lg:pb-4 border-[#84A7A1]">
+            Resepsi
+          </button>
         </div>
         <div className="text-center space-y-4 mb-16 text-[#84a7a1]">
-          <h1 className="text-3xl font-semibold">
-            Intimate Virtual
-            <br />
+          <h1 className="text-3xl lg:text-4xl font-semibold">
+            Intimate Virtual <br className="lg:hidden" />
             Reception
           </h1>
-          <p className="font-medium text-xl">
+          <p className="font-medium text-xl lg:text-2xl">
             We invite you to the following <br />
             Virtual Wedding Reception
           </p>
         </div>
-        <div className="space-y-4 font-semibold">
-          <h1 className="text-center text-4xl">Akad nikah</h1>
-          <div className="flex items-center justify-center gap-4">
-            <span className="text-xl py-2 border-y-2 border-[#DBBB85]">
-              T U E S D A Y
-            </span>
-            <span className="text-8xl">8</span>
-            <span className="text-xl py-2 border-y-2 border-[#DBBB85]">
-              O C T O B E R
-            </span>
+        <div className="mb-12 lg:mb-20 lg:flex lg:justify-center lg:gap-16">
+          <div className="space-y-4 font-semibold lg:space-y-8">
+            <h1 className="text-center text-4xl">Akad nikah</h1>
+            <div className="flex lg:flex-col items-center justify-center gap-4 lg:gap-8">
+              <span className="text-xl py-2 border-y lg:text-3xl border-[#DBBB85] lg:font-semibold">
+                T U E S D A Y
+              </span>
+              <span className="text-8xl">8</span>
+              <span className="text-xl py-2 border-y lg:text-3xl border-[#DBBB85] lg:font-semibold">
+                O C T O B E R
+              </span>
+            </div>
+            <p className="text-center text-3xl">09:00 - 12:00</p>
           </div>
-          <p className="text-center text-3xl">09:00 - 12:00</p>
-        </div>
-        <hr className="my-12 border border-[#84A7A1]" />
-        <div className="space-y-4 font-semibold mb-12">
-          <h1 className="text-center text-4xl">Resepsi</h1>
-          <div className="flex items-center justify-center gap-4">
-            <span className="text-xl py-2 border-y-2 border-[#DBBB85]">
-              T U E S D A Y
-            </span>
-            <span className="text-8xl">8</span>
-            <span className="text-xl py-2 border-y-2 border-[#DBBB85]">
-              O C T O B E R
-            </span>
+          <div className="my-12 h-[2px] lg:w-[1.5px] lg:h-[405px] lg:my-0 bg-[#84A7A1]" />
+          <div className="space-y-4 font-semibold lg:space-y-8">
+            <h1 className="text-center text-4xl">Resepsi</h1>
+            <div className="flex lg:flex-col items-center justify-center gap-4 lg:gap-8">
+              <span className="text-xl py-2 border-y lg:text-3xl border-[#DBBB85] lg:font-semibold">
+                T U E S D A Y
+              </span>
+              <span className="text-8xl">8</span>
+              <span className="text-xl py-2 border-y lg:text-3xl border-[#DBBB85] lg:font-semibold">
+                O C T O B E R
+              </span>
+            </div>
+            <p className="text-center text-3xl">16:00 - 21:00</p>
           </div>
-          <p className="text-center text-3xl">16:00 - 21:00</p>
         </div>
         <div className="flex justify-center">
           <button className="rounded-md border-2 border-[#D5AF6F] bg-[#D5AF6F] text-[#003C4C] py-3 px-6 text-2xl font-bold">
